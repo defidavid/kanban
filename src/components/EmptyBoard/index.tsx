d9b5d9bf9ba6665d1185572f190ef70d2ebd7abd
David Salgado <david.b.salgado@gmail.com>
2022-05-02 20:04:59 -0500

    Empty board padding

--- src/components/EmptyBoard/index.tsx
+++ src/components/EmptyBoard/index.tsx
@@ -7,11 +7,11 @@ export default function EmptyBoard({ onClick }: { onClick: () => void }): JSX.El
   const { t } = useTranslation();
 
   return (
-    <Box display="flex" pt={15} margin={"0 auto"} alignItems="center" flexDirection="column">
+    <Box display="flex" pt={15} pr={20} pl={20} margin={"0 auto"} alignItems="center" flexDirection="column">
       <Typography variant="h5" color="textSecondary" textAlign="center">
         {t("messages.EmptyBoard")}
       </Typography>
-      <Box mt={2}>
+      <Box mt={4}>
         <Button variant="contained" onClick={onClick}>
           {t("words.AddColumn")}
         </Button>
